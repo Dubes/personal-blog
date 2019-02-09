f35503d29d727b28c98297e2cfa00ee14e6c2185
Dubes <Dubes@users.noreply.github.com>
2019-02-09 20:47:57 +0100

    prettier changes

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,29 +1,22 @@
-import Typography from "typography"
+import Typography from 'typography'
 
 const typography = new Typography({
-  baseFontSize: "18px",
+  baseFontSize: '18px',
   baseLineHeight: 1.666,
   headerFontFamily: [
-    "Merriweather",
-    "Helvetica Neue",
-    "sans-serif",
-    "Segoe UI",
-    "Helvetica",
-    "Arial",
+    'Merriweather',
+    'Helvetica Neue',
+    'sans-serif',
+    'Segoe UI',
+    'Helvetica',
+    'Arial',
   ],
   headerWeight: 700,
-  bodyFontFamily: ["Merriweather", "serif"],
+  bodyFontFamily: ['Merriweather', 'serif'],
   googleFonts: [
     {
       name: 'Merriweather',
-      styles: [
-        '400',
-        '400i',        
-        '700',
-        '700i',
-        '900',
-        '900i',
-      ],
+      styles: ['400', '400i', '700', '700i', '900', '900i'],
     },
   ],
 })
